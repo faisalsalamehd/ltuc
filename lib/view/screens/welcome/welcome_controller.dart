31bd5711504a533d2de933d92aec1bae9364a095
faisalsalamehd <faisal1991sam@gmail.com>
2025-08-17 18:10:52 +0300

    feat: Implement Firebase authentication with sign up and sign in functionality

--- lib/view/screens/welcome/welcome_controller.dart
+++ lib/view/screens/welcome/welcome_controller.dart
@@ -1,3 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class WelcomeController extends GetxController {}
+class WelcomeController extends GetxController {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isObsecure = true;
+
+  void signUp() async {
+    final auth = FirebaseAuth.instance;
+
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      Get.snackbar('Error', 'Please fill all the fields');
+      return;
+    }
+
+    try {
+      auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void signIn() async {
+    final auth = FirebaseAuth.instance;
+
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      Get.snackbar('Error', 'Please fill all the fields');
+      return;
+    }
+
+    try {
+      auth.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+}
--- lib/view/screens/welcome/welcome_screen.dart
+++ lib/view/screens/welcome/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ltuc/helpers/notification_manager.dart';
@@ -7,6 +8,7 @@ import 'package:ltuc/util/constant/general.dart';
 import 'package:ltuc/util/translations/trasnsaltion_key.dart';
 import 'package:ltuc/view/screens/welcome/welcome_controller.dart';
 import 'package:ltuc/view/widgets/primary_button/primary_button.dart';
+import 'package:ltuc/view/widgets/text_field.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -18,21 +20,35 @@ class WelcomeScreen extends StatelessWidget {
         return Scaffold(
           body: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Center(
-              child: PrimaryButton(
-                onTap: () async {
-                  log('message');
-                  NotificationManagement.sendNotification(
-                    fcmToken!,
-                    {'type': 'cart'},
-                    {
-                      'title': TransltionKey.signin.tr,
-                      'body': 'Welcome to our app!',
-                    },
-                  );
-                },
-                title: TransltionKey.signin.tr,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MainTextField(
+                  hint: "Email", 
+                  textInputType: TextInputType.emailAddress,
+                 controller: ctrl.emailController,
+                ),
+                SizedBox(height: 10,), 
+                MainTextField(
+                  obscureText: ctrl.isObsecure,
+                  hint: "Password", 
+                  textInputType: TextInputType.visiblePassword,
+                 controller: ctrl.passwordController,
+                ),
+                SizedBox(height: 16),
+                PrimaryButton(
+                  onTap: ()  {
+
+                    //  ctrl.signIn();
+                    if(FirebaseAuth.instance.currentUser != null){
+                      // Get.toNamed("/home");
+                      log("Success");
+                    }
+                    
+                  },
+                  title: TransltionKey.signin.tr,
+                ),
+              ],
             ),
           ),
         );
